a3a2c5c6a96f337a71d87c2483d9f83f350a390a
ahmadirvandi7 <92698735+ahmadirvandi7@users.noreply.github.com>
2022-06-15 12:03:08 +0700

    Update Queue.cpp

--- Queue.cpp
+++ Queue.cpp
@@ -14,21 +14,32 @@ void tampilan(queue<int>noAntrian){
 }
 
 int main (){
-int n,angka,pilih;
-queue<int>bioskop;
+	int n,angka,pilih;
+	queue<int>bioskop;
 
-do{
- system("cls");
-   cout<<"|===========================|\n";
-   cout<<"| Antrian Tiket bioskop     |\n";
-   cout<<"|===========================|\n";
-   cout<<"|1. Tambah Antrian          |"<<endl;
-   cout<<"|2. Panggil Antrian         |"<<endl;
-   cout<<"|3. Daftar Antrian          |"<<endl;
-   cout<<"|4. Keluar                  |"<<endl;
-   cout<<"|===========================|"<<endl;
-   cout<<"Pilih : ";
-   cin>>pilih;
+	do{
+ 	system("cls");
+   	cout<<"|===========================|\n";
+   	cout<<"| Antrian Tiket bioskop     |\n";
+   	cout<<"|===========================|\n";
+   	cout<<"|1. Tambah Antrian          |"<<endl;
+   	cout<<"|2. Panggil Antrian         |"<<endl;
+   	cout<<"|3. Daftar Antrian          |"<<endl;
+   	cout<<"|4. Keluar                  |"<<endl;
+   	cout<<"|===========================|"<<endl;
+   	cout<<"Pilih : ";
+   	cin>>pilih;
+		
+		switch (pilih){
+		case 1 :
+			cout<<"Jumlah Antrian :";
+			cin>>n;
+			for(int i=0 ; i<n ;i++){
+				cout<<"Nomor Antrian ke - "<<i+1<<" ";
+				cin>>angka;
+				bioskop.push(angka);
+			}
+			break;
 
 return 0;
 }
