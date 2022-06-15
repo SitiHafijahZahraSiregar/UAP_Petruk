2204612eaf77abe8fcb06ee09d2a1ce9d584cae4
SyailendraWangsa <107517581+SyailendraWangsa@users.noreply.github.com>
2022-06-15 11:52:44 +0700

    Update Queue.cpp

--- Queue.cpp
+++ Queue.cpp
@@ -2,12 +2,33 @@
 #include <iostream>
 #include <queue>
 #include <conio.h>
-
 using namespace std;
 
+
+void tampilan(queue<int>noAntrian){
+   while(!noAntrian.empty()){
+	cout<<"No Antrian :"<<noAntrian.front()<<endl;
+	noAntrian.pop();
+	}
+	cout<<endl;
+}
+
 int main (){
+int n,angka,pilih;
+queue<int>bioskop;
 
-
+do{
+ system("cls");
+   cout<<"|===========================|\n";
+   cout<<"| Antrian Tiket bioskop     |\n";
+   cout<<"|===========================|\n";
+   cout<<"|1. Tambah Antrian          |"<<endl;
+   cout<<"|2. Panggil Antrian         |"<<endl;
+   cout<<"|3. Daftar Antrian          |"<<endl;
+   cout<<"|4. Keluar                  |"<<endl;
+   cout<<"|===========================|"<<endl;
+   cout<<"Pilih : ";
+   cin>>pilih;
 
 return 0;
 }
